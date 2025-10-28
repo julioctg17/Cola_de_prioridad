eef9334ab1ee6cc05a6d8a9491adf982da7cdf4e
Julio Trinidad <utp0211635@alumno.utpuebla.edu.mx>
2025-10-28 10:40:30 -0600

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Cola_de_prioridad
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.btnEncolar = new System.Windows.Forms.Button();
+            this.btnDesencolar = new System.Windows.Forms.Button();
+            this.lstCola = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número";
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(43, 86);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(259, 22);
+            this.txtNumero.TabIndex = 1;
+            // 
+            // btnEncolar
+            // 
+            this.btnEncolar.Location = new System.Drawing.Point(43, 146);
+            this.btnEncolar.Name = "btnEncolar";
+            this.btnEncolar.Size = new System.Drawing.Size(120, 40);
+            this.btnEncolar.TabIndex = 2;
+            this.btnEncolar.Text = "Encolar";
+            this.btnEncolar.UseVisualStyleBackColor = true;
+            this.btnEncolar.Click += new System.EventHandler(this.btnEncolar_Click);
+            // 
+            // btnDesencolar
+            // 
+            this.btnDesencolar.Location = new System.Drawing.Point(182, 146);
+            this.btnDesencolar.Name = "btnDesencolar";
+            this.btnDesencolar.Size = new System.Drawing.Size(120, 40);
+            this.btnDesencolar.TabIndex = 3;
+            this.btnDesencolar.Text = "Desencolar";
+            this.btnDesencolar.UseVisualStyleBackColor = true;
+            this.btnDesencolar.Click += new System.EventHandler(this.btnDesencolar_Click);
+            // 
+            // lstCola
+            // 
+            this.lstCola.FormattingEnabled = true;
+            this.lstCola.ItemHeight = 16;
+            this.lstCola.Location = new System.Drawing.Point(43, 210);
+            this.lstCola.Name = "lstCola";
+            this.lstCola.Size = new System.Drawing.Size(259, 84);
+            this.lstCola.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 359);
+            this.Controls.Add(this.lstCola);
+            this.Controls.Add(this.btnDesencolar);
+            this.Controls.Add(this.btnEncolar);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.Button btnEncolar;
+        private System.Windows.Forms.Button btnDesencolar;
+        private System.Windows.Forms.ListBox lstCola;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cola_de_prioridad
+{
+    public partial class Form1 : Form
+    {
+        private List<int> colaPrioridad = new List<int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEncolar_Click(object sender, EventArgs e)
+        {
+            // Validar entrada
+            if (int.TryParse(txtNumero.Text, out int valor))
+            {
+                colaPrioridad.Add(valor);
+                ActualizarLista();
+                txtNumero.Clear();
+                txtNumero.Focus();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese un número válido.");
+            }
+
+        }
+
+        private void btnDesencolar_Click(object sender, EventArgs e)
+        {
+            if (colaPrioridad.Count == 0)
+            {
+                MessageBox.Show("La cola está vacía.");
+                return;
+            }
+
+            // Buscar el número mayor (mayor prioridad)
+            int mayor = colaPrioridad.Max();
+            colaPrioridad.Remove(mayor);
+
+            MessageBox.Show($"Se atendió el elemento con prioridad: {mayor}");
+            ActualizarLista();
+
+        }
+        private void ActualizarLista()
+        {
+            // Mostrar cola ordenada (de mayor a menor)
+            lstCola.Items.Clear();
+            foreach (int n in colaPrioridad.OrderByDescending(x => x))
+            {
+                lstCola.Items.Add(n);
+            }
+        }
+    }
+}
